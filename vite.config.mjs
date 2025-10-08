9fc8557245f51dcad8520feac9892537e5ad983d
Gianni Ballerini <Gianniballerini@gmail.com>
2025-10-08 19:57:00 -0300

    update fonts path

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ const useHttps = process.env.VITE_USE_HTTPS === 'true';
 export default defineConfig({
   root: 'src',
   base: '/webview/',
+  publicDir: '../public/',
   plugins: [
     glsl(),
     vitePugPlugin({ pugLocals: {
@@ -22,17 +23,17 @@ export default defineConfig({
         server.middlewares.use((req, res, next) => {
           const url = req.url || '';
           const extensions = ['mp3', 'mp4', 'webm', 'glb', 'jpg', 'png', 'webp', 'hdr', 'json', 'gltf', 'xml', 'json']
-          
+
           // Check if the url contains a dot (.) which means it's probably a static file request
           if (url.includes('.') && extensions.includes(url.split('.').pop())) {
             const filePath = path.join(process.cwd(), 'public', url);
-            
+
             // Check if the file exists in the public directory
             if (!fs.existsSync(filePath)) {
               res.statusCode = 404;
               return;
             }
-          } 
+          }
           next(); // Continue to the next middleware if the file exists
         })
       }
